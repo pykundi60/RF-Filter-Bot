8ec7cda806e34d2f54f692cc74e19b24488dda08
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-03-10 18:00:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 <b>Here Is The Help For My Commands.</b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➥ Creator: <a href='https://t.me/zautekm'>Zaute Km</a>
+➥ Creator: <a href='https://t.me/Y2say'>Dhanush TG</a>
 ➥ Library: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
 ➥ Language: Python 𝟹
 ➥ Data Base: <a href='https://www.mongodb.com/'>MongoDB</a>
@@ -20,21 +20,18 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 ➥ Build Status: v2.0.1 [ Beta ]"""
 
     SOURCE_TXT = """<b>Source:</b>
-IMDb is a Open source project.
-Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 👈</a>
+Dhanuah is not a Open source project.
 
 <b>DEVS:</b>
-- <a href='https://t.me/josprojects'>Jos Projects</a>
-
-<b>SUPPORT GROUP</b>
-- <a href='https://t.me/+y53tWFUw6Q43NzE9'>Jos Movie Club</a>"""
+- <a href='https://t.me/Naveen_TG'>Naveen-TG</a>
+"""
 
     MANUALFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Dingdi will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. IMDb should have admin privillage.
+1. Dhanush should have admin privillage.
 2. Only admins can add filters in a chat.
 3. Alert buttons have a limit of 64 characters.
 
@@ -46,11 +43,11 @@ Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 
 
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- tgmoviebot support both url and alert inline buttons.
+- Dhanush support both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. IMDb supports buttons with any telegram media type.
+2. Dhanush supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format.
 
 <b>URL buttons:</b>
@@ -113,7 +110,7 @@ Paste some texts or documents on a website!
 • /paste [reply] - paste the replied text on Pasty
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -125,7 +122,7 @@ Do as you wish with telegra.ph module!
 • /tgmedia or /tgraph - upload supported media (within 5MB) to telegraph.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -139,7 +136,7 @@ Get information about something!
 • /json - get the json details of a message.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhansuh should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -149,7 +146,7 @@ Get information about something!
 • /torrent or /tor <movie name>: Get Your Torrent Link From Various Resource.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -161,9 +158,9 @@ Translate texts to a specific language!
 • /tr [language code][reply] - translate replied message to specific language.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
-• IMDb can translate texts to 200+ languages."""
+• Dhanush can translate texts to 200+ languages."""
 
     SEARCH_TXT = """Help: <b>IMDb</b>
 
@@ -174,7 +171,7 @@ Search many things without leaving telegram!
 • /search  - get the film information from various sources.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • More search tools can be found on inline.
 • Those commands works on both pm and group."""
 
@@ -186,7 +183,7 @@ Need to delete lots of messages? That's what purges are for!
 • /purge - delete all messages from the replied to message, to the current message.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on group.
 • These commands can be used by Only admin."""
 
@@ -208,7 +205,7 @@ This module allows you to do that easily, by exposing some common actions, so ev
 -> <code>/tmute @username 2h</code>
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on group.
 • These commands can be used by Only admin."""
 
@@ -221,7 +218,7 @@ All the pin related commands can be found here; keep your chat up to date on the
 • /unpin: Unpin the current pinned message. If used as a reply, unpins the replied to message.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works only group.
 • These commands can be used by Only admin."""
 
@@ -267,7 +264,7 @@ Some dank memes for fun or whatever!
 • /runs strings
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -282,7 +279,7 @@ Some URLs is Shortner
 <code>/short https://t.me/josprojects</code>
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -294,7 +291,7 @@ A module to convert text to voice with language support.
 • /tts - Reply to any text message with language code to convert as audio.
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -311,7 +308,7 @@ Music download modules, for those who love music.
 <b>Example:</b> <code>/ytthumb https://youtu.be/h6PtzFYaMxQ</code>
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -327,7 +324,7 @@ There Is Nothing To Know More. Send Me The Limit Of Your Password.
 • Only Digits Are Allowed
 • Maximum Allowed Digits Till 84 
 (I Can't Generate Passwords Above The Length 84)
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
@@ -339,7 +336,7 @@ a bot to create a link to share text in the telegram.
 • /share (text or reply to message)
 
 <b>NOTE:</b>
-• IMDb should have admin privillage.
+• Dhanush should have admin privillage.
 • These commands works on both pm and group.
 • These commands can be used by any group member."""
 
