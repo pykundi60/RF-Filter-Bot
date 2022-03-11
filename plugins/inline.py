319a77df82413d528d54aa93667797a85577cba8
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-03-11 16:00:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,13 +22,6 @@ async def inline_users(query: InlineQuery):
 @Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
-    
-    if not await inline_users(query):
-        await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='okDa',
-                           switch_pm_parameter="hehe")
-        return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
