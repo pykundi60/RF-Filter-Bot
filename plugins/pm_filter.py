68fa5bed655ecd6bee8ecae4f2449d6750310b01
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-03-11 10:35:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -95,10 +95,11 @@ async def give_filter(client,message):
                 break 
    
     else:
-         if FILTER_MODE.get(str(message.chat.id)) == "False":
-             return
-         else:
-             await auto_filter(client, message)   
+        if FILTER_MODE.get(str(message.chat.id)) == "False":
+            return
+        else:
+            await auto_filter(client, message)   
+
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
