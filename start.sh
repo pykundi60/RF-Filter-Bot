f3fef058bbbf2bb751f4572ae7440e1b36d348a6
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-11 15:32:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Naveen-TG/Dhanush.git /Dhanush
-cd /Dhanush
+git clone -b master https://github.com/Hmfhacker/HMF-movies-V2.git /HMF-movies-V2
+cd /HMF-movies-V2
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
