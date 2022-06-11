63f0c0a25732dce8be93297a69c6aab7d606e036
Hmfhacker <95634169+Hmfhacker@users.noreply.github.com>
2022-06-11 12:19:04 +0530

    Update yt_thumb_dl.py

--- plugins/josprojects/yt_thumb_dl.py
+++ plugins/josprojects/yt_thumb_dl.py
@@ -40,7 +40,7 @@ async def send_thumbnail(bot, update):
         )
         await update.reply_photo(
             photo=thumbnail,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('𝙈𝙤𝙫𝙞𝙚 𝙂𝙧𝙤𝙪𝙥', url='https://telegram.me/TamilMV_Collections')]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ🚨', url='https://telegram.me/PlayBoysDXD')]]),
             quote=True
         )
         await message.delete()
@@ -48,5 +48,5 @@ async def send_thumbnail(bot, update):
         await message.edit_text(
             text="**Please Use** /ytthumb (youtube link)\n\n**Example:** `/ytthumb https://youtu.be/h6PtzFYaMxQ`",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('𝙈𝙤𝙫𝙞𝙚 𝙂𝙧𝙤𝙪𝙥', url='https://telegram.me/TamilMV_Collections')]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ🚨', url='https://telegram.me/PlayBoysDXD')]])
         )
