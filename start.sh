457fb55b4cd8cfb6b0c35c98a05cdd7f3a790bd0
bavabee <93858110+bavabee@users.noreply.github.com>
2022-12-15 07:57:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/bavabee/TamizhmastersBot-V2.git /TamizhmastersBot-V2
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/bavabee/TamizhmastersBot-V2.git /TamizhmastersBot-V2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TamizhmastersBot-V2
+fi
 cd /TamizhmastersBot-V2
-echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Bot...."
 python3 bot.py
 
