8df65af36e7d89f94611d6c892d652e3c325faca
pykundi60 <122137102+pykundi60@users.noreply.github.com>
2023-01-08 09:03:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/bavabee/RF-Filter-Bot.git /RF-Filter-Bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RF-Filter-Bot
-fi
-cd /TamizhmastersBot-V2
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo """██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ██╗███╗   ██╗ ██████╗ 
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██║████╗  ██║██╔════╝ 
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     ██║██╔██╗ ██║██║  ███╗
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██║██║╚██╗██║██║   ██║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║╚██████╔╝
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+ """
 python3 bot.py
 
